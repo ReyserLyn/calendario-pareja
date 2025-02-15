0be25e25e2e6bd452f3d072fa60652f333b7cb33
Reyser Zapata <reyserlyn@gmail.com>
2025-02-15 03:01:33 -0500

    fix problem lint in component box

--- src/components/ui/Box.tsx
+++ src/components/ui/Box.tsx
@@ -1,13 +1,12 @@
 import { cn } from "@/lib/utils";
 import React from "react";
 
-interface BoxProps extends React.HTMLAttributes<HTMLDivElement> {}
-
-const Box = React.forwardRef<HTMLDivElement, BoxProps>(
-  ({ className, ...props }, ref) => (
-    <div className={cn(className)} ref={ref} {...props} />
-  )
-);
+const Box = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div className={cn(className)} ref={ref} {...props} />
+));
 
 Box.displayName = "Box";
 
