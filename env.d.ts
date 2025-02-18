d6b1305f1dfdd08dba88bdf2aaaf1355a66972c8
Reyser Zapata <reyserlyn@gmail.com>
2025-02-17 19:31:09 -0500

    deleted comment

--- env.d.ts
+++ env.d.ts
@@ -1,8 +1,5 @@
-// env.d.ts
 declare namespace NodeJS {
   interface ProcessEnv {
     readonly NEXT_PUBLIC_POCKETBASE_URL: string;
-    readonly PB_ADMIN_EMAIL: string;
-    readonly PB_ADMIN_PASSWORD: string;
   }
 }
